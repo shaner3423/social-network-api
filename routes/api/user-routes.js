c2a225a39fdb32eb63e5c22f6a3608280d74c412
Sugar Shane <scrisostomo17@gmail.com>
2021-10-01 15:33:23 -0400

    test

---
+++ routes/api/user-routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/user-controller');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
